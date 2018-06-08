d12a60034f05c76cdc54244dd7c1af3030209760
Jordan Kline <jordan.kline@gmail.com>
2018-06-08 11:17:21 -0700

    Done.

--- src/components/TweetWall.js
+++ src/components/TweetWall.js
@@ -10,9 +10,26 @@ class TweetWall extends React.Component {
     };
   }
 
-  // TODO: componentWillMount()
-  // TODO: shouldComponentUpdate()
-  // TODO: componentWillReceiveProps()
+  componentWillMount = () => {
+    this.setState({
+      tweets: this.props.newTweets,
+    })
+  }
+  
+  shouldComponentUpdate = (nextProps) => {
+    return Boolean(nextProps.newTweets.length > 0);
+  }
+  
+  componentWillReceiveProps = (nextProps) => {
+    let allTweets = this.state.tweets;
+    nextProps.newTweets.forEach(tweet => {
+      return allTweets.unshift(tweet);
+    })
+    
+    this.setState({
+      tweets: allTweets,
+    })
+  }
 
   render() {
     const tweets = this.state.tweets.map((tweet, index) => <Tweet text={tweet.text} key={index} />);
